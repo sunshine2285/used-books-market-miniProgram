a8e312c21e4e9094bd863b0544100c1dea2a4610
sunshine2285 <sunshine2285@163.com>
2020-06-02 12:45:42 +0800

    完善service

---
+++ server/src/main/java/com/ouc/usedbook/service/UsedbookService.java
@@ -0,0 +1,13 @@
+package com.ouc.usedbook.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @author 孙浩杰
+ * @version 1.0
+ * @description TODO
+ * @date 2020/6/2 12:44
+ */
+@Service
+public class UsedbookService {
+}
---
+++ server/src/main/java/com/ouc/usedbook/service/UserService.java
@@ -0,0 +1,14 @@
+package com.ouc.usedbook.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @author 孙浩杰
+ * @version 1.0
+ * @description TODO
+ * @date 2020/6/2 12:45
+ */
+@Service
+public class UserService {
+    
+}
